19d740d005944612720725f1252cdea0cbb7fe1b
Alexandre <75760260+AlexandreMig@users.noreply.github.com>
2022-10-21 16:38:10 -0400

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './components/user-login/user-login/user-login.component';
+import { HeaderComponent } from './components/header/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
